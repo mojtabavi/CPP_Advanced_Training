952a5735febd6daed4428fd441a5b7ad1df60231
Ali Mojtabavi <mojtabavi99@yahoo.com>
2020-10-18 21:21:54 +0330

    start training

---
+++ cmath.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "ceil(9.2): " << ceil(9.2) << endl; //rounds x to the smallest integer not less then x
+    cout << "ceil(-9.2): " << ceil(-9.2) << endl;
+
+    cout << "cos(0): " << cos(0) << endl; //rounds x to the smallest integer not less then x
+
+}
+
