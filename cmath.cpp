9f8e4993a2afa014e9a43ae6dd287d6168c8bbba
Ali Mojtabavi <mojtabavi99@yahoo.com>
2020-10-18 21:37:21 +0330

    Work with CMATH library

--- cmath.cpp
+++ cmath.cpp
@@ -7,7 +7,27 @@ int main()
     cout << "ceil(9.2): " << ceil(9.2) << endl; //rounds x to the smallest integer not less then x
     cout << "ceil(-9.2): " << ceil(-9.2) << endl;
 
-    cout << "cos(0): " << cos(0) << endl; //rounds x to the smallest integer not less then x
+    cout << "cos(0): " << cos(0) << endl; //cosine of x
+
+    cout << "exp(1.0): " << exp(1.0) << endl; //exponential function e^x
+
+    cout << "fabs(-8.76): " << fabs(-8.76) << endl; //absolute value of x
+
+    cout << "floor(9.2): " << floor(9.2) << endl; //rounds x to the largest integer not greater then x
+
+    cout << "fmod(2.6, 1.2): " << fmod(2.6, 1.2) << endl; //remainder of x/y as a floating point number
+
+    cout << "log(2.718282): " << log(2.718282) << endl; //natural logarithm of x (base e)
+
+    cout << "log10(100.0): " << log10(100.0) << endl; //logarithm of x (base10)
+    
+    cout << "pow(2,7): " << pow(2,7) << endl; //pow(x,y) x^y
+
+    cout << "sin(0.0): " << sin(0.0) << endl; //sine of x
+
+    cout << "sqrt(9.0): " << sqrt(9.0) << endl; //square root of x
+
+    cout << "tan(0.0): " << tan(0.0) << endl; //trigonometric tangent of x
 
 }
 
