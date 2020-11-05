9734bc1bbba698f989055e16319d368c980cfe87
Seyed Ali Mojtabavi <mojtabavi99@yahoo.com>
2020-11-06 03:25:47 +0330

    Initializing and using a reference.

---
+++ Reference/reference.cpp
@@ -0,0 +1,14 @@
+// Initializing and using a reference
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 3;
+    int &y = x; // y refers to (is an alias for) x
+
+    cout << "x = " << x << endl << "y = " << y << endl;
+    y = 7; // actually modifies x
+    cout << "x = " << x << endl << "y = " << y << endl;
+    
+}// end main
