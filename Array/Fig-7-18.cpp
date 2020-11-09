38e8f04ef7ca76a93c7a4165e2643b3828f8bde2
Seyed Ali Mojtabavi <mojtabavi99@yahoo.com>
2020-11-10 03:04:59 +0330

    Linear Search of an array

---
+++ Array/Fig-7-18.cpp
@@ -0,0 +1,35 @@
+// Linear search of an array
+#include <iostream>
+using namespace std;
+
+int linearSearch( const int [], int, int );
+
+int main()
+{
+    const int arraySize = 100;
+    int a[ arraySize ];
+    int searchKey;
+
+    for ( int i = 0; i < arraySize; ++i )
+        a[ i ] = 2 * i;
+
+    cout << "Enter integer search key: ";
+    cin >> searchKey;
+
+    int element = linearSearch( a, searchKey, arraySize );
+
+    if ( element != -1 )
+        cout << "Found value in element " << element << endl;
+    else
+        cout << "Value not found" << endl;
+}// end main
+
+
+int linearSearch( const int array[], int key, int sizeOfArray )
+{
+    for ( int j = 0; j < sizeOfArray; ++j )
+        if ( array[ j ] == key )
+            return j;
+
+    return -1;
+}
